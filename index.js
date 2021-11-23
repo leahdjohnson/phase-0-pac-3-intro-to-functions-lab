bcd8abbcb4d0c3fabbb97cc95c0030a5787e2ca6
Leah <elloworldt@gmail.com>
2021-11-23 09:06:41 -0800

    completed assignment

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+
+  function logShout(string) {
+    console.log(string.toUpperCase());
+  }
+
+  function logWhisper(string) {
+   console.log(string.toLowerCase());
+  }
+
+function sayHiToGrandma(message = "Hi, Grandma.") {
+if (whisper) 
+  console.log("I can't hear you!");        
+} else { (shout)
+  console.log("YES, INDEED!");
+} else if { 
+    (message = "I love you, Grandma.");
+    console.log("I love you, too.");
+}
